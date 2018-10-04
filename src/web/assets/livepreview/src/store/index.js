771fb6e74fc54d817321dde31c65c7c24b3e6fd7
Michael Hüneburg <hello@michaelhue.com>
2018-10-04 23:45:29 +0200

    Port store getters and actions

--- src/web/assets/livepreview/src/store/index.js
+++ src/web/assets/livepreview/src/store/index.js
@@ -1,11 +1,12 @@
 import { Store } from "vuex";
+import { clamp, percent } from "../util";
 
 const state = {
   enabled: true,
   presets: [],
   zoomLevels: [],
-  currentPreset: -1,
-  zoom: -1,
+  selectedPreset: -1,
+  selectedZoom: -1,
   x: 0,
   y: 0,
   presetX: 0,
@@ -24,18 +25,97 @@ export function createStore(initial = {}) {
 
       presetOptions: state => state.presets.map(([name]) => name),
 
-      zoomOptions: state => state.zoomLevels.map(x => Math.round(x * 100) + "%")
+      zoomOptions: state =>
+        state.zoomLevels.map(x => Math.round(x * 100) + "%"),
+
+      hasPreset: state => state.selectedPreset > -1,
+
+      presetSize: state => ({ x: state.presetX, y: state.presetY }),
+
+      customSize: state => ({ x: state.x, y: state.y }),
+
+      size: (_, getters) =>
+        getters.hasPreset
+          ? { ...getters.presetSize }
+          : { ...getters.customSize },
+
+      zoom: state => state.zoomLevels[state.selectedZoom] || 0,
+
+      fitScale: (state, getters) =>
+        Math.min(
+          1.0,
+          state.stageX / getters.size.x,
+          state.stageY / getters.size.y
+        ) || 1.0,
+
+      fitZoom: (_, getters) => percent(getters.fitScale),
+
+      scale: (_, getters) => getters.zoom || getters.fitScale
     },
 
     actions: {
       toggle({ commit, getters }) {
         commit("setEnabled", !getters.enabled);
+      },
+
+      setPreset({ commit, state }, index = -1) {
+        const [, x, y] = state.presets[index] || [null, 0, 0];
+        commit("setSelectedPreset", index);
+        commit("setPresetSize", { x, y });
+      },
+
+      setZoom({ commit }, index = -1) {
+        commit("setSelectedZoom", index);
+      },
+
+      setCustomSize({ commit, state }, { x, y }) {
+        const range = clamp(100)(9999);
+        return commit("setCustomSize", {
+          x: range(x) || state.x,
+          y: range(y) || state.y
+        });
+      },
+
+      updateStageSize({ commit, state }, size) {
+        commit("setStageSize", size);
+
+        if (state.x === 0 && state.y === 0) {
+          commit("setCustomSize", size);
+        }
+      },
+
+      rotate({ commit, getters }) {
+        const action = getters.hasPreset ? "setPresetSize" : "setCustomSize";
+        commit(action, { x: getters.size.y, y: getters.size.x });
       }
     },
 
     mutations: {
       setEnabled(state, value) {
         state.enabled = !!value;
+      },
+
+      setSelectedPreset(state, index) {
+        state.selectedPreset = index;
+      },
+
+      setSelectedZoom(state, index) {
+        state.selectedZoom = index;
+      },
+
+      setCustomSize(state, { x, y }) {
+        state.x = x;
+        state.y = y;
+      },
+
+      setPresetSize(state, { x, y }) {
+        state.presetX = x;
+        state.presetY = y;
+      },
+
+      setStageSize(state, { x, y }) {
+        state.stageX = x;
+        state.stageY = y;
       }
     }
   });
