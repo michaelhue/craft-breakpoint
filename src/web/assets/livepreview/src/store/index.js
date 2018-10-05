741f5b60eb6c34d0350d72c5d22a963932cd9d8a
Michael Hüneburg <hello@michaelhue.com>
2018-10-06 00:46:12 +0200

    Remove custom size from persisted paths

--- src/web/assets/livepreview/src/store/index.js
+++ src/web/assets/livepreview/src/store/index.js
@@ -43,9 +43,7 @@ export function createStore(initial = {}) {
           "selectedPreset",
           "selectedZoom",
           "presetX",
-          "presetY",
-          "x",
-          "y"
+          "presetY"
         ]
       })
     ]
